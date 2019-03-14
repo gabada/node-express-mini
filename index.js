93479f797dcc614c57750a858d9e7b80a17078f8
Gill Abada <gill@callifi.com>
2019-03-14 13:22:17 -0400

    install dependencies, start server, complete GET.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+
+const port = 5000;
+const server = express();
+
+server.post('/api/users',(req, res) => {
+    res.send('Hello World from Express!');
+});
+
+server.get('/api/users',(req, res) => {
+    db
+        .find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+});
+
+server.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+})
